44c9d42ca57be0125c1d898140b6d17fa4c17994
Danielis Joniskis <g00333859@gmit.ie>
2017-11-28 22:39:45 +0000

    matching first set of inputs

--- eliza.go
+++ eliza.go
@@ -8,10 +8,21 @@ import(
 )
 
 func ElizaResponse(input string) string{
+	if matched, _ := regexp.MatchString(`(?i).*\bFrench\b.*`, input); matched {
+		return "France is a beautiful country"
+	}
+	if matched, _ := regexp.MatchString(`(?i).*\bweekend\b.*`, input); matched {
+		return "I love the weekend!"
+	}
+	if matched, _ := regexp.MatchString(`(?i).*I was.*`, input); matched {
+		return "Thats very nice"
+	}
+	if matched, _ := regexp.MatchString(`(?i).*father was.*`, input); matched {
+		return "A teacher! Tell me more about him?"
+	}
 	if matched, _ := regexp.MatchString(`(?i).*\bfather\b.*`, input); matched {
 		return "Why don’t you tell me more about your father?"
 	}
-
 	//This randomly returns one of the following strings if nothing is matched.
 	answers := []string{
 		"I’m not sure what you’re trying to say. Could you explain it to me?",
@@ -27,6 +38,22 @@ func main() {
 
 	fmt.Println("People say I look like both my mother and father.")
 	fmt.Println(ElizaResponse("People say I look like both my mother and father."))
+	fmt.Println()
+
+	fmt.Println("Father was a teacher.")
+	fmt.Println(ElizaResponse("Father was a teacher."))
+	fmt.Println()
+
+	fmt.Println("I was my father’s favourite.")
+	fmt.Println(ElizaResponse("I was my father’s favourite."))
+	fmt.Println()
+
+	fmt.Println("I’m looking forward to the weekend.")
+	fmt.Println(ElizaResponse("I’m looking forward to the weekend."))
+	fmt.Println()
+
+	fmt.Println("My grandfather was French!")
+	fmt.Println(ElizaResponse("My grandfather was French!"))
 	fmt.Println()
 
 }
